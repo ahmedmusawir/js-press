ac4ab48493db7fab14b8b7cb2cd141f8e390e04b
Ahmed Musawir <moose@Mooses-High-Sierra.local>
2018-09-23 16:02:35 +0800

    Google Auth done - 23 Sep 2018

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('./keys');
+//LOAD USER MODEL
+const User = mongoose.model('users');
 
 module.exports = passport => {
   passport.use(
@@ -12,12 +14,40 @@ module.exports = passport => {
         proxy: true //for heroku https
       },
       (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken);
-        console.log(profile);
-        // User.findOrCreate({ googleId: profile.id }, function(err, user) {
-        //   return cb(err, user);
-        // });
+        // console.log(accessToken);
+        // console.log(profile);
+
+        const image = profile.photos[0].value.substring(
+          0,
+          profile.photos[0].value.indexOf('?')
+        );
+        const newUser = {
+          googleID: profile.id,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          email: profile.emails[0].value,
+          image: image
+        };
+
+        //CHECK FOR EXISTING USER
+        User.findOne({ googleID: profile.id }).then(user => {
+          if (user) {
+            //RETURN USER
+            done(null, user);
+          } else {
+            //CREATE NEW USER
+            new User(newUser).save().then(user => done(null, user));
+          }
+        });
       }
     )
   );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => done(null, user));
+  });
 };
--- index.js
+++ index.js
@@ -1,21 +1,74 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+//LOAD USER MODEL
+require('./models/User');
 
 //PASSPORT CONFIG
 require('./config/passport')(passport);
 
 //LOAD ROUTES
 const auth = require('./routes/auth');
+const verify = require('./routes/auth');
+const logout = require('./routes/auth');
 
+//LOAD KEYS
+const keys = require('./config/keys');
+
+//GETS RID OF DEPRICATION WARNINGS
+mongoose.Promise = global.Promise;
+
+//DB CONFIG
+// const db = require('./config/database');
+
+//CONNECTING TO MONGO DB WITH MONGOOSE
+mongoose
+  .connect(
+    keys.mongoURI,
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log('Connected to MongoDB ...'))
+  .catch(err => console.error('Could not connect to MongoDB ...'));
+
+//APP INIT
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('It Works!');
 });
 
+//COOKIE PARSER
+app.use(cookieParser());
+
+//SESSION MIDDLEWARE
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+    // cookie: { maxAge: 60000 }
+  })
+);
+
+//PASSPORT MIDDLEWARE
+app.use(passport.initialize());
+app.use(passport.session());
+
+//GLOBAL VARS
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
+
 //USE ROUTES
 app.use('/auth', auth);
+app.use('/auth/verify', verify);
+app.use('/auth/logout', logout);
 
 const port = process.env.PORT || 5000;
 
